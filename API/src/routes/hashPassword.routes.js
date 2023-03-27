ecb80057980598d6801fd7a6d3f8c056c0fc7ddb
itsrusty <rusty3031@gmail.com>
2023-03-26 17:57:04 -0700

    add route to get data password

--- API/src/routes/hashPassword.routes.js
+++ API/src/routes/hashPassword.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { hashPassword } from "../controllers/hashPassword.js";
 
 const router = Router();
 
-router.get("/hash", (req, res) => {
-    res.json({ message: "Hello World!" });
-})
+router.get("/hash/:password", hashPassword)
 
-
+router.get("/", (req, res) => {
+  res.json({ message: "nada" });
+});
 
 export default router;
